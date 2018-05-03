c1dc35ddd2773f0c2169bd1116c59f1f11847679
Kevin Beam <kbeam@nsidc.org>
2018-05-03 11:17:41 -0600

    Add basic layout for the page

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,55 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.css';
 
-class CMR extends React.Component {
+
+class SpatialUI extends React.Component {
     render() {
         return (
-            <div className="everest-stuff">
-              <div>
-                <span>Hey there!</span>
-              </div>
+            <div>
+              <CollectionSelector/>
+              <div id="globe"></div>
+              <button type="button" name="Go!" placeholder="">Go!</button>
+              <GranuleList/>
             </div>
         );
+    }
+}
+
+class CollectionSelector extends React.Component {
+    render() {
+        return (
+            <select className="dropdown" name="collections">
+              <option>IDHDT4</option>
+              <option>ILATM1B</option>
+              <option>ILVIS2</option>
+            </select>
+        )
+    }
+}
+
+class GranuleList extends React.Component {
+    render() {
+        return (
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>ID</th>
+                  <th>Summary</th>
+                  <th>Granules</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+        )
     }
 }
 
 // ========================================
 
 ReactDOM.render(
-    <CMR />,
+    <SpatialUI />,
     document.getElementById('everest-ui')
 );
