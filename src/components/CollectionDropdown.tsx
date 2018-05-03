d04f732801f5d383bf6ae8be217dc0ba76740fa6
Matt Fisher <matt.fisher@nsidc.org>
2018-05-03 12:45:41 -0600

    Stolen from EVEREST-2

--- src/components/CollectionDropdown.tsx
+++ src/components/CollectionDropdown.tsx
@@ -1,13 +1,42 @@
 import * as React from "react";
 
-class Component extends React.Component {
-  render() {
-    return (
-      <div id="collection-container">
-        <h3>Select a collection</h3>
-      </div>
-    );
-  }
+const CMR_COLLECTION_URL = 'https://cmr.earthdata.nasa.gov/search/collections.json?page_size=500&provider=NSIDC_ECS&sort_key=short_name';
+
+interface CollectionsState {
+    collections: any
+}
+
+class Component extends React.Component<{}, CollectionsState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            collections: null
+        }
+    }
+
+    componentDidMount() {
+        fetch(CMR_COLLECTION_URL)
+            .then(response => response.json())
+            .then(response => this.setState({
+                collections: response.feed.entry
+            }));
+    }
+
+    render() {
+        let collectionOptions = null;
+
+        if (this.state.collections) {
+            collectionOptions = this.state.collections.map((c:any) => (
+                <option>{c.dataset_id}</option>
+            ));
+        }
+
+        return (
+            <select className="dropdown" name="collections">
+              {collectionOptions}
+            </select>
+        )
+    }
 }
 
 export default Component;
