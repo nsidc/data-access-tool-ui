2268720592c88b95469265a9699f7b6b7ec55ab1
Matt Fisher <matt.fisher@nsidc.org>
2018-05-08 15:15:05 -0600

    Add comment about Drupal.behaviors

--- src/index.tsx
+++ src/index.tsx
@@ -87,6 +87,7 @@ const renderApp = () => {
 
 declare var Drupal: any;
 
+// If the app is being rendered in Drupal, wait for the page to load first
 if (typeof(Drupal) !== "undefined") {
   Drupal.behaviors.AppBehavior = {
     attach: function(context: any, settings: any) {
