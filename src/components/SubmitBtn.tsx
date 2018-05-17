43e181adcf1a4aacfbaaf11d8b211e46a9a3142b
Matt Fisher <matt.fisher@nsidc.org>
2018-05-17 17:54:04 -0600

    Extract CMR calls to their own module

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -1,7 +1,8 @@
 import * as moment from "moment";
 import * as React from "react";
 
-const CMR_GRANULE_URL = "https://cmr.earthdata.nasa.gov/search/granules.json?page_size=50&provider=NSIDC_ECS&sort_key=short_name";
+import { granuleRequest } from "../CMR";
+
 
 interface SubmitButtonProps {
   collectionId: string;
@@ -20,7 +21,6 @@ class Component extends React.Component<SubmitButtonProps, SubmitButtonState> {
   constructor(props: any) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.cmrRequest = this.cmrRequest.bind(this);
     this.handleCmrResponse = this.handleCmrResponse.bind(this);
     this.state = {
       cmrResponse: null
@@ -32,20 +32,10 @@ class Component extends React.Component<SubmitButtonProps, SubmitButtonState> {
     this.props.onGranuleResponse(this.state.cmrResponse);
   }
 
-
-  cmrRequest(collectionId: string, temporalLowerBound: moment.Moment, temporalUpperBound: moment.Moment) {
-    const URL = CMR_GRANULE_URL
-      + `&concept_id=${this.props.collectionId}`
-      + `&temporal\[\]=${temporalLowerBound.utc().format()},${temporalUpperBound.utc().format()}`;
-    console.log(`Request to CMR with: ${collectionId}, ${temporalLowerBound}, ${temporalUpperBound}:\n  ${URL}`);
-    fetch(URL)
-        .then(response => response.json())
-        .then(json => this.handleCmrResponse(json));
-  }
-
   handleClick() {
     if (this.props.collectionId && this.props.temporalLowerBound && this.props.temporalUpperBound) {
-      this.cmrRequest(this.props.collectionId, this.props.temporalLowerBound, this.props.temporalUpperBound);
+      granuleRequest(this.props.collectionId, this.props.temporalLowerBound, this.props.temporalUpperBound)
+        .then(json => this.handleCmrResponse(json));
     } else {
       console.log("Insufficient props provided.");
     }
