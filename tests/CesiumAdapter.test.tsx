1cbbc49df6d52b2616f87a544121c7a0c7194753
collinsj <collinsj@nsidc.org>
2018-05-30 15:15:13 -0600

    Silence console.log in test.

--- tests/CesiumAdapter.test.tsx
+++ tests/CesiumAdapter.test.tsx
@@ -2,8 +2,8 @@ const Cesium = require("cesium/Cesium");
 
 describe("Cesium adapter", () => {
   test("Creates Cesium viewer", () => {
-      // The only current purpose of this test is to demonstrate mocking.
-      // Uncomment the "console" 
-    console.log(Cesium);
+    // The only current purpose of this test is to demonstrate mocking.
+    // Uncomment the "console.log" to see the innards of the mocked class. 
+    //console.log(Cesium);
   });
 });
