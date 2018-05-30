818d1a6d8628e1973adf298d18b18816d76e5b59
collinsj <collinsj@nsidc.org>
2018-05-30 14:02:14 -0600

    add CircleCI config

--- tests/CesiumAdapter.test.tsx
+++ tests/CesiumAdapter.test.tsx
@@ -2,6 +2,8 @@ const Cesium = require("cesium/Cesium");
 
 describe("Cesium adapter", () => {
   test("Creates Cesium viewer", () => {
+      // The only current purpose of this test is to demonstrate mocking.
+      // Uncomment the "console" 
     console.log(Cesium);
   });
 });
