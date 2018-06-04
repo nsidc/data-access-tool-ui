e54c535fc3db9042162928edca8896565fc4a915
Matt Fisher <matt.fisher@nsidc.org>
2018-06-04 13:21:43 -0600

    Trailing whitespace

--- __mocks__/cesium/cesium.tsx
+++ __mocks__/cesium/cesium.tsx
@@ -2,7 +2,7 @@ export const Color = jest.fn();
 export const Ellipsoid = jest.fn();
 
 const mock = jest.fn().mockImplementation(() => {
-    return { 
+    return {
       mockColor: Color,
       mockEllipsoid: Ellipsoid,
     };
