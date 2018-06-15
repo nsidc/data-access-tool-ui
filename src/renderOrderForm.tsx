d3db013b16b555b59e2bccc9c625fa02fabb34b5
Matt Fisher <matt.fisher@nsidc.org>
2018-06-15 09:06:30 -0600

    Remove react-modal

--- src/renderOrderForm.tsx
+++ src/renderOrderForm.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as ReactModal from "react-modal";
 
 import "./index.css";
 
 import { EverestUI } from "./components/EverestUI";
 
 export const renderApp = () => {
-  ReactModal.setAppElement("#everest-ui");
   ReactDOM.render(
       <EverestUI />,
       document.getElementById("everest-ui"),
