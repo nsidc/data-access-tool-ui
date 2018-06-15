a6161be6c838b608238fa710390459078d3dffe5
Kevin Beam <kbeam@nsidc.org>
2018-06-15 14:29:09 -0600

    Simplify index and profile code.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 declare var Drupal: any;
 
-let renderUI: any;
+import { renderApp } from "./renderOrderForm";
+
 if (typeof(Drupal) !== "undefined") {
   // By extending Drupal.behaviors with a new behavior and callback, we can
   // ensure that the "everest-ui" element and required Drupal state exist
   // before we render the app or include dependencies.
   Drupal.behaviors.EverestUI = {
-    attach: (context: any, settings: any) => {
-      /* tslint:disable:no-var-requires */
-      renderUI = require("./renderOrderForm");
-      /* tslint:enable:no-var-requires */
-      return renderUI.renderApp();
-    },
+    attach: (context: any, settings: any) => renderApp(),
   };
 } else {
-  /* tslint:disable:no-var-requires */
-  renderUI = require("./renderOrderForm");
-  /* tslint:enable:no-var-requires */
-  renderUI.renderApp();
+  renderApp();
 }
--- src/profile.ts
+++ src/profile.ts
@@ -1,21 +1,14 @@
 declare var Drupal: any;
 
-let renderProfile: any;
+import { renderApp } from "./renderProfile";
+
 if (typeof(Drupal) !== "undefined") {
   // By extending Drupal.behaviors with a new behavior and callback, we can
   // ensure that the "everest-ui-profile" element and required Drupal state
   // exist before we render the app or include dependencies.
   Drupal.behaviors.EverestProfile = {
-    attach: (context: any, settings: any) => {
-      /* tslint:disable:no-var-requires */
-      renderProfile = require("./renderProfile");
-      /* tslint:enable:no-var-requires */
-      return renderProfile.renderApp();
-    },
+    attach: (context: any, settings: any) => renderApp(),
   };
 } else {
-  /* tslint:disable:no-var-requires */
-  renderProfile = require("./renderProfile");
-  /* tslint:enable:no-var-requires */
-  renderProfile.renderApp();
+  renderApp();
 }
