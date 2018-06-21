ad6c648a96911c7ed5b95258c75371c996fc80ae
Michael Brandt <michaelbrandt5@gmail.com>
2018-06-21 17:11:23 -0600

    render white circles on clicked points on the map

--- src/utils/PolygonMode.ts
+++ src/utils/PolygonMode.ts
@@ -1,9 +1,13 @@
+import * as dragImg from "../img/dragIcon.png";
+
 /* tslint:disable:no-var-requires */
 const Cesium = require("cesium/Cesium");
 /* tslint:enable:no-var-requires */
 
 export class PolygonMode {
 
+  private billboards: any[] = [];
+  private billboardCollection: any;
   private ellipsoid: any;
   private finishedDrawingCallback: any;
   private minPoints = 3;
@@ -30,6 +34,8 @@ export class PolygonMode {
 
     this.mouseHandler.setInputAction(this.onMouseMove.bind(this),
                                      Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+    this.billboardCollection = this.scene.primitives.add(new Cesium.BillboardCollection());
   }
 
   public endMode() {
@@ -39,13 +45,17 @@ export class PolygonMode {
   }
 
   private render() {
+    // gather all the points; rendering doesn't care about the distinction
+    // between clicked points and the point following the cursor
     let points = this.points;
     if (this.mousePoint !== null) {
       points = points.concat([this.mousePoint]);
     }
     points = points.map((p) => p.cartesianXYZ);
 
+    // if we meet the minimum points requirement, render the polygon
     if (points.length >= this.minPoints) {
+      // remove previously rendered polygon
       if (this.polygon) {
         this.scene.primitives.remove(this.polygon);
         Cesium.destroyObject(this.polygon);
@@ -54,7 +64,6 @@ export class PolygonMode {
       const appearance = new Cesium.EllipsoidSurfaceAppearance({
         aboveGround: false,
       });
-
       const geometry = Cesium.PolygonGeometry.fromPositions({
         ellipsoid: this.ellipsoid,
         positions: points,
@@ -63,7 +72,6 @@ export class PolygonMode {
       const geometryInstances = new Cesium.GeometryInstance({
         geometry,
       });
-
       this.polygon = new Cesium.Primitive({
         appearance,
         asynchronous: false,
@@ -71,6 +79,9 @@ export class PolygonMode {
       });
       this.scene.primitives.add(this.polygon);
     }
+
+    // if exactly one point has been set, render a line between that point and
+    // the current mouse point
   }
 
   private screenPositionToPoint(position: any) {
@@ -92,17 +103,42 @@ export class PolygonMode {
     if (point === null) { return; }
 
     this.points.push(point);
+    this.addBillboard(point);
+  }
+
+  private popPoint() {
+    this.points.pop();
+    this.removeLastBillboard();
+  }
+
+  private addBillboard(point: any) {
+    const billboard = this.billboardCollection.add({
+      image: dragImg,
+      position: point.cartesianXYZ,
+    });
+    this.billboards.push(billboard);
+  }
+
+  private removeLastBillboard() {
+    const billboard = this.billboards.pop();
+    this.billboardCollection.remove(billboard);
   }
 
   private updateMousePoint(position: any) {
     const point = this.screenPositionToPoint(position);
     if (point === null) { return; }
 
+    this.clearMousePoint();
     this.mousePoint = point;
+    this.addBillboard(point);
   }
 
   private clearMousePoint() {
     this.mousePoint = null;
+
+    if (this.billboards.length === (this.points.length + 1)) {
+      this.removeLastBillboard();
+    }
   }
 
   private onLeftClick({position}: any) {
@@ -115,7 +151,7 @@ export class PolygonMode {
     // two individual left click events fire before the double click does; this
     // results in a duplicate of the final position at the end of
     // `this.points` that can (and should) be safely removed
-    this.points.pop();
+    this.popPoint();
 
     if (this.points.length >= this.minPoints) {
       this.clearMousePoint();
