68734c8aaccf5e3ab331c2baf0137798d597de3a
Michael Brandt <michaelbrandt5@gmail.com>
2018-07-31 15:54:55 -0600

    Revert "remove all mock comments from CMR.ts"
    This reverts commit 7dcf5576224b4145db2ebb33fc53de0690a0a249.

--- src/utils/CMR.ts
+++ src/utils/CMR.ts
@@ -1,3 +1,4 @@
+// import * as fetchMock from "fetch-mock";
 import * as moment from "moment";
 
 import { IGeoJsonBbox, IGeoJsonPolygon } from "../types/GeoJson";
@@ -31,7 +32,11 @@ const spatialParameter = (geoJSON: IGeoJsonPolygon): string => {
   return `&${param}=${value}`;
 };
 
+// the lines in this file containing `fetchMock` may be uncommented during
+// development to simulate CMR being down
 export const cmrStatusRequest = ({onFailure, onSuccess}: any) => {
+  // fetchMock.mock(CMR_STATUS_URL, 500);
+
   const fetchResult = fetch(CMR_STATUS_URL, {
     headers: cmrHeaders,
   })
@@ -42,6 +47,8 @@ export const cmrStatusRequest = ({onFailure, onSuccess}: any) => {
         onFailure(response);
       }
     });
+
+  // fetchMock.restore();
 
   return fetchResult;
 };
