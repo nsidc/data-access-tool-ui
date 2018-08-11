b30f7631704d8272d05dd9076066551b16dd5e44
Matt Fisher <matt.fisher@nsidc.org>
2018-08-10 21:17:26 -0600

    Make Modals screenreader-friendly

--- src/renderOrderForm.tsx
+++ src/renderOrderForm.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as ReactModal from "react-modal";
 
 import "./css/index.css";
 
 import { EverestUI } from "./components/EverestUI";
+
+ReactModal.setAppElement("#everest-ui");
 
 export const renderApp = (environment: any) => {
   ReactDOM.render(
