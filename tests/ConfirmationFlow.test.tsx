923fce29a57134d0aac9f96ab0719de647427e64
Michael Brandt <michaelbrandt5@gmail.com>
2018-08-15 09:40:08 -0600

    make spinner test more specific

--- tests/ConfirmationFlow.test.tsx
+++ tests/ConfirmationFlow.test.tsx
@@ -13,8 +13,6 @@ shim();
 
 const environment = setupEnvironment(false);
 
-fetchMock.mock(environment.urls.hermesOrderUrl, 200, {method: "POST"});
-
 const setup = (props = {}) => {
 
   const defaultProps = {
@@ -37,29 +35,33 @@ describe("ConfirmationFlow", () => {
     expect(component.find(OrderConfirmationContent).length).toEqual(1);
   });
 
-  test("shows spinner while order request is unresolved", async () => {
-    expect.assertions(2);
-
-    const mockShowLoadingIcon = jest.fn();
-    ConfirmationFlow.prototype.showLoadingIcon = mockShowLoadingIcon;
-
-    const mockHandleOrderResponse = jest.fn();
-    ConfirmationFlow.prototype.handleOrderResponse = mockHandleOrderResponse;
-
-    const mockHandleOrderError = jest.fn();
-    ConfirmationFlow.prototype.handleOrderError = mockHandleOrderError;
-
-    const component = setup({
-      orderSubmissionParameters: new OrderSubmissionParameters(),
-      orderType: OrderTypes.listOfLinks,
+  describe("with a failing submit order request", () => {
+    beforeAll(() => {
+      fetchMock.mock(environment.urls.hermesOrderUrl, 500, {method: "POST"});
     });
 
-    return component.instance().handleConfirmationClick().finally(() => {
-      expect(mockShowLoadingIcon).toHaveBeenCalled();
+    afterAll(() => {
+      fetchMock.restore();
+    });
 
-      const numCalls = (mockedFunc: any) => mockedFunc.mock.calls.length;
-      const orderHandlerCalls = numCalls(mockHandleOrderError) + numCalls(mockHandleOrderResponse);
-      expect(orderHandlerCalls).toEqual(1);
+    test("shows spinner while and then handles the error", async () => {
+      expect.assertions(2);
+
+      const mockShowLoadingIcon = jest.fn();
+      ConfirmationFlow.prototype.showLoadingIcon = mockShowLoadingIcon;
+
+      const mockHandleOrderError = jest.fn();
+      ConfirmationFlow.prototype.handleOrderError = mockHandleOrderError;
+
+      const component = setup({
+        orderSubmissionParameters: new OrderSubmissionParameters(),
+        orderType: OrderTypes.listOfLinks,
+      });
+
+      return component.instance().handleConfirmationClick().finally(() => {
+        expect(mockShowLoadingIcon).toHaveBeenCalled();
+        expect(mockHandleOrderError).toHaveBeenCalled();
+      });
     });
   });
 });
