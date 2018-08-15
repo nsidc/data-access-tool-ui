d5fdbc64c0cc44738ff8b402f84eb6133bf7c0a0
Kevin Beam <kbeam@nsidc.org>
2018-08-14 23:47:51 -0600

    WIP: Initial version with GIBS imagery

--- src/utils/CesiumAdapter.ts
+++ src/utils/CesiumAdapter.ts
@@ -8,6 +8,95 @@ const Cesium = require("cesium/Cesium");
 require("cesium/Widgets/widgets.css");
 /* tslint:enable:no-var-requires */
 
+const gibsGeographicTilingScheme = () => {
+  const ggts = new Cesium.GeographicTilingScheme();
+
+  const tilePixels = 512;
+  const rectangle = Cesium.Rectangle.MAX_VALUE;
+
+  // Resolution: radians per pixel
+  const levels = [
+    { width: 2, height: 1, resolution: 0.009817477042468103 },
+    { width: 3, height: 2, resolution: 0.004908738521234052 },
+    { width: 5, height: 3, resolution: 0.002454369260617026 },
+    { width: 10, height: 5, resolution: 0.001227184630308513 },
+    { width: 20, height: 10, resolution: 0.0006135923151542565 },
+    { width: 40, height: 20, resolution: 0.00030679615757712823 },
+    { width: 80, height: 40, resolution: 0.00015339807878856412 },
+    { width: 160, height: 80, resolution: 0.00007669903939428206 },
+    { width: 320, height: 160, resolution: 0.00003834951969714103 },
+  ];
+
+  ggts.getNumberOfXTilesAtLevel = (level: number) => {
+    const result = levels[level].width;
+    return result;
+  };
+
+  ggts.getNumberOfYTilesAtLevel = (level: number) => {
+    const result = levels[level].height;
+    return result;
+  };
+
+  ggts.tileXYToRectangle = (x: number, y: number, level: number, result: any) => {
+    const resolution = levels[level].resolution;
+
+    const xTileWidth = resolution * tilePixels;
+    const west = x * xTileWidth + rectangle.west;
+    const east = (x + 1) * xTileWidth + rectangle.west;
+
+    const yTileHeight = resolution * tilePixels;
+    const north = rectangle.north - y * yTileHeight;
+    const south = rectangle.north - (y + 1) * yTileHeight;
+
+    if (!result) {
+      result = new Cesium.Rectangle(0, 0, 0, 0);
+    }
+    result.west = west;
+    result.south = south;
+    result.east = east;
+    result.north = north;
+    return result;
+  };
+
+  ggts.positionToTileXY = (position: any, level: number, result: any) => {
+    if (!Cesium.Rectangle.contains(rectangle, position)) {
+      return undefined;
+    }
+
+    const xTiles = levels[level].width;
+    const yTiles = levels[level].height;
+    const resolution = levels[level].resolution;
+
+    const xTileWidth = resolution * tilePixels;
+    const yTileHeight = resolution * tilePixels;
+
+    let longitude = position.longitude;
+    if (rectangle.east < rectangle.west) {
+      longitude += Cesium.Math.TWO_PI;
+    }
+
+    let xTileCoordinate = Math.floor((longitude - rectangle.west) / xTileWidth);
+    if (xTileCoordinate >= xTiles) {
+      xTileCoordinate = xTiles - 1;
+    }
+
+    const latitude = position.latitude;
+    let yTileCoordinate = Math.floor((rectangle.north - latitude) / yTileHeight);
+    if (yTileCoordinate > yTiles) {
+      yTileCoordinate = yTiles - 1;
+    }
+
+    if (!result) {
+      result = new Cesium.Cartesian2(0, 0);
+    }
+    result.x = xTileCoordinate;
+    result.y = yTileCoordinate;
+    return result;
+  };
+
+  return ggts;
+};
+
 export class CesiumAdapter {
   private static extentColor = new Cesium.Color(0.0, 1.0, 1.0, 0.5);
   private static ellipsoid = Cesium.Ellipsoid.WGS84;
@@ -22,6 +111,19 @@ export class CesiumAdapter {
   }
 
   public createViewer(elementId: string, spatialSelection: IGeoJsonPolygon) {
+    const gibsProvider = new Cesium.WebMapTileServiceImageryProvider({
+      format: "image/jpeg",
+      layer: "BlueMarble_ShadedRelief_Bathymetry",
+      maximumLevel: 7,
+      minimumLevel: 0,
+      style: "",
+      tileHeight: 512,
+      tileMatrixSetID: "EPSG4326_500m",
+      tileWidth: 512,
+      tilingScheme: gibsGeographicTilingScheme(),
+      url: "//gibs.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi",
+    });
+
     this.viewer = new Cesium.Viewer(elementId, {
       animation: false,
       baseLayerPicker: false,
@@ -29,6 +131,7 @@ export class CesiumAdapter {
       fullscreenButton: false,
       geocoder: false,
       homeButton: false,
+      imageryProvider: gibsProvider,
       infoBox: false,
       navigationHelpButton: false,
       navigationInstructionsInitiallyVisible: false,
