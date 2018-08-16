3b2ce1aaebd8cd0991e1becc4852744bd04c48d8
Matt Fisher <matt.fisher@nsidc.org>
2018-08-16 15:21:56 -0600

    Fix typo :bell: :bell: :bell:

--- tests/CMR.test.tsx
+++ tests/CMR.test.tsx
@@ -1,13 +1,13 @@
 import { versionParameters } from "../src/utils/CMR";
 
 describe("CMR version_id query parameters", () => {
-  it("should be correctly genreated for one-digit version_ids", () => {
+  it("should be correctly generated for one-digit version_ids", () => {
     expect(versionParameters(1)).toBe("version=1&version=01&version=001");
   });
-  it("should be correctly genreated for two-digit version_ids", () => {
+  it("should be correctly generated for two-digit version_ids", () => {
     expect(versionParameters(12)).toBe("version=12&version=012");
   });
-  it("should be correctly genreated for three-digit version_ids", () => {
+  it("should be correctly generated for three-digit version_ids", () => {
     expect(versionParameters(123)).toBe("version=123");
   });
 });
