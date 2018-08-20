baaa3d411e3e031f54d3faa28d3c255b21db8802
collinsj <collinsj@nsidc.org>
2018-08-20 11:10:22 -0600

    Refactor help text into separate component.

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import "../css/index.css";
-import * as resetImg from "../img/glyphicons_067_cleaning.png";
-import * as polygonImg from "../img/glyphicons_096_vector_path_polygon.png";
 import { IGeoJsonPolygon } from "../types/GeoJson";
 import { CesiumAdapter } from "../utils/CesiumAdapter";
 import { hasChanged } from "../utils/hasChanged";
+import { HelpText } from "./HelpText";
 import { SpatialSelectionToolbar } from "./SpatialSelectionToolbar";
 
 interface IGlobeProps {
@@ -35,26 +34,9 @@ export class Globe extends React.Component<IGlobeProps, {}> {
   }
 
   public render() {
-    // Is there a better way of managing the icons and help text, besides simply
-    // embedding them here?
     return (
       <div id="spatial-selection">
-        <div className="help-text">
-          <h3>Limit spatially by drawing a polygon:</h3>
-          <span>Note: Green overlay shows coverage, unless global.</span>
-          <section>
-          <ul id="left-column">
-            <li>Size: Scroll or two-finger drag</li>
-            <li>Rotation: Click and drag globe</li>
-            <li>Begin: Click <img src={polygonImg} alt="the polygon" /> icon to start</li>
-          </ul>
-          <ul id="right-column">
-            <li>Draw: Click on desired points</li>
-            <li>Finish drawing: Double-click</li>
-            <li>Clear: Click <img src={resetImg} alt="the cleanup" /> icon</li>
-          </ul>
-          </section>
-        </div>
+        <HelpText />
         <div id={this.elementId}>
           <SpatialSelectionToolbar
             onClickPolygon={() => {
