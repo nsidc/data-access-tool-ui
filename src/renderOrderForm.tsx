bacb0edd1df20c6a6928fa85e3f62f5ae8c51547
Michael Brandt <michaelbrandt5@gmail.com>
2018-08-27 11:06:27 -0600

    namespace styling under #everest-ui, change ReactModal's rendering
    Styling before was namespaced with `#everest-container`, now with `#everest-ui`.
    
    DOM before:
    
    ```
    <body>
      <div id="everest-ui">
        <div id="everest-container" />
      </div>
      <div class="ReactModalPortal" />
    </body>
    ```
    
    DOM after:
    
    ```
    <body>
      <div id="everest-ui">
        <div id="everest-container" />
        <div class="ReactModalPortal" />
      </div>
    </body>
    ```

--- src/renderOrderForm.tsx
+++ src/renderOrderForm.tsx
@@ -6,8 +6,16 @@ import "./styles/index.less";
 
 import { EverestUI } from "./components/EverestUI";
 
+// Render an empty everest-container so that it can be passed to ReactModal
+// before the whole app has rendered. The app element set here should not be a
+// parent of the modal, which is set via ReactModal.parentSelector in
+// ConfirmationFlow. With this setup, #everest-ui is a parent of both
+// div#everest-container and div.ReactModalPortal.
+//
 // http://reactcommunity.org/react-modal/accessibility/#app-element
-ReactModal.setAppElement("#everest-ui");
+// http://reactcommunity.org/react-modal/#custom-parent
+ReactDOM.render((<div id="everest-container" />), document.getElementById("everest-ui"));
+ReactModal.setAppElement("#everest-container");
 
 export const renderApp = (environment: any) => {
   ReactDOM.render(
