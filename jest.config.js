093d8257b5158a8c26984efd506958cbf7c6a4b9
Michael Brandt <michaelbrandt5@gmail.com>
2018-09-29 13:22:40 -0600

    fix jest config for Globe tests
    since adding @types/cesium, Cesium is imported with "cesium" rather than
    "cesium/Cesium", so the jest config should match the new string

--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
         "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    "cesium/Cesium": "<rootDir>/__mocks__/cesium/cesium.tsx"
+    "cesium": "<rootDir>/__mocks__/cesium/cesium.tsx"
   }
 }
