a333ac21c8a7293f0de8c6be8ba738136f99e2ab
Matt Fisher <matt.fisher@nsidc.org>
2018-10-09 12:18:43 -0600

    Add tests for state module

---
+++ tests/state.test.ts
@@ -0,0 +1,59 @@
+import { List, Map } from "immutable";
+import * as moment from "moment";
+
+import { CmrGranule } from "../src/types/CmrGranule";
+import { OrderParameters } from "../src/types/OrderParameters";
+import { OrderSubmissionParameters } from "../src/types/OrderSubmissionParameters";
+import { updateStateInitGranules, updateStateAddGranules } from "../src/utils/state";
+
+const granuleInput = {
+  dataset_id: "id1",
+  granule_size: "",
+  links: List([Map({href: "href1"})]),
+  producer_granule_id: "",
+  time_start: moment(),
+  time_end: moment(),
+  title: "Barry",
+};
+
+const initialState = {
+  cmrGranuleCount: undefined,
+  cmrGranuleScrollDepleted: false,
+  cmrGranuleScrollId: undefined,
+  cmrGranules: List<CmrGranule>(),
+  cmrLoadingGranuleInit: false,
+  cmrLoadingGranuleScroll: false,
+  cmrStatusChecked: false,
+  cmrStatusOk: false,
+  loadedParamsFromLocalStorage: false,
+  orderParameters: new OrderParameters(),
+  orderSubmissionParameters: undefined,
+  stateCanBeFrozen: false,
+};
+
+const expected = {
+  cmrGranules: List([
+    new CmrGranule(granuleInput),
+  ]),
+  orderSubmissionParameters: new OrderSubmissionParameters({
+    collectionInfo: List([List([
+      "id1",
+      "href1",
+    ])]),
+    granuleURs: List(["Barry"])
+  }),
+};
+
+describe("State updater", () => {
+  test("initializes granules", () => {
+    const actual = updateStateInitGranules([granuleInput])();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("appends granules", () => {
+    const actual = updateStateAddGranules([granuleInput])(initialState);
+
+    expect(actual).toEqual(expected);
+  });
+});
