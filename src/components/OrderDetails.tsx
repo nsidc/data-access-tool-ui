0300773d634d96eaf97afdf88f80118b21d3ee13
Michael Brandt <michaelbrandt5@gmail.com>
2019-04-22 07:53:14 -0600

    parse file_urls instead of links from the Hermes API response

--- src/components/OrderDetails.tsx
+++ src/components/OrderDetails.tsx
@@ -88,25 +88,26 @@ export class OrderDetails extends React.Component<IOrderDetailsProps, IOrderDeta
     if (["inprogress", "expired"].includes(order.status)) {
       return [];
     }
-    const zipLink = this.findZipLink(order);
-    const filteredLinks = order.links.filter((i: any) => i !== zipLink);
 
-    const links = filteredLinks.map((link: any, index: number) => {
-      return ( <li key={index}><a href={link.uri}>{link.uri}</a></li> );
+    const links = order.file_urls.data.map((link: any, index: number) => {
+      return ( <li key={index}><a href={link}>{link}</a></li> );
     });
     return links;
   }
 
   private buildZipLink(order: any): JSX.Element {
-    const zipLink = this.findZipLink(order);
-    if (!zipLink) {
+    const zipLinks = this.findZipLinks(order);
+    if (!zipLinks) {
       return ( <span>Please wait...</span> );
     }
-    return ( <a href={zipLink.uri}>{zipLink.uri}</a> );
+
+    const zipLink = zipLinks[0];
+
+    return ( <a href={zipLink}>{zipLink}</a> );
   }
 
-  private findZipLink(order: any): any {
-    return order.links.find((link: any) => link.uri.match(/\.zip$/));
+  private findZipLinks(order: any): any {
+    return order.file_urls.archive;
   }
 
   private loadOrder = () => {
