96b90c3d0630b968686f8d2c909878bb4ed3e22b
Chris Torrence <chris.torrence@nsidc.org>
2019-10-24 12:31:15 -0600

    Really fix test

--- tests/CMR.test.tsx
+++ tests/CMR.test.tsx
@@ -90,8 +90,8 @@ describe("CMR spatial parameters", () => {
 describe("cmrBoxArrToSpatialSelection", () => {
   const globalSpatialSelection = BoundingBox.global();
   it("returns a global box for an empty array", () => {
-    expect(cmrBoxArrToSpatialSelection([]).rect).toBe(globalSpatialSelection.rect);
-    expect(cmrBoxArrToSpatialSelection(List([])).rect).toBe(globalSpatialSelection.rect);
+    expect(cmrBoxArrToSpatialSelection([]).rect).toEqual(globalSpatialSelection.rect);
+    expect(cmrBoxArrToSpatialSelection(List([])).rect).toEqual(globalSpatialSelection.rect);
   });
 
   it("returns the smallest box that encompasses all given boxes", () => {
